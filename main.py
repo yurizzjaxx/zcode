a9a33e4853b0384120442579e8e06a13889fa2c6
yuriZzJaxx <132253210+yurizzjaxx@users.noreply.github.com>
2025-03-08 18:49:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
+import request
+
 print("""
 
-                         **       ********             **                          
-  **   **               //       //////**             /**                          
- //** **  **   ** ****** **           **   ******     /**  ******   **   ** **   **
-  //***  /**  /**//**//*/**          **   ////**      /** //////** //** ** //** ** 
-   /**   /**  /** /** / /**         **       **       /**  *******  //***   //***  
-   **    /**  /** /**   /**        **       **    **  /** **////**   **/**   **/** 
-  **     //******/***   /**       ******** ******//***** //******** ** //** ** //**
- //       ////// ///    //       //////// //////  /////   //////// //   // //   // 
-                                                                                   
-                                                                                   
-                                                                                   
-                                                                                   
-                                                                                   
-                                                                                   
-                                                                                   
-                                                                                   
- ********                       **                                                 
-//////**                       /**                                                 
-     **    *****   ******      /**  *****                                          
-    **    **///** **////**  ****** **///**                                         
-   **    /**  // /**   /** **///**/*******                                         
-  **     /**   **/**   /**/**  /**/**////                                          
- ********//***** //****** //******//******                                         
-////////  /////   //////   //////  //////                                          
-
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+YYYYYYY       YYYYYYY                                      iiii       ZZZZZZZZZZZZZZZZZZZ                           JJJJJJJJJJJ                                                        
+Y:::::Y       Y:::::Y                                     i::::i      Z:::::::::::::::::Z                           J:::::::::J                                                        
+Y:::::Y       Y:::::Y                                      iiii       Z:::::::::::::::::Z                           J:::::::::J                                                        
+Y::::::Y     Y::::::Y                                                 Z:::ZZZZZZZZ:::::Z                            JJ:::::::JJ                                                        
+YYY:::::Y   Y:::::YYYuuuuuu    uuuuuu rrrrr   rrrrrrrrr  iiiiiii      ZZZZZ     Z:::::Z  zzzzzzzzzzzzzzzzz            J:::::J  aaaaaaaaaaaaa   xxxxxxx      xxxxxxxxxxxxxx      xxxxxxx
+   Y:::::Y Y:::::Y   u::::u    u::::u r::::rrr:::::::::r i:::::i              Z:::::Z    z:::::::::::::::z            J:::::J  a::::::::::::a   x:::::x    x:::::x  x:::::x    x:::::x 
+    Y:::::Y:::::Y    u::::u    u::::u r:::::::::::::::::r i::::i             Z:::::Z     z::::::::::::::z             J:::::J  aaaaaaaaa:::::a   x:::::x  x:::::x    x:::::x  x:::::x  
+     Y:::::::::Y     u::::u    u::::u rr::::::rrrrr::::::ri::::i            Z:::::Z      zzzzzzzz::::::z              J:::::j           a::::a    x:::::xx:::::x      x:::::xx:::::x   
+      Y:::::::Y      u::::u    u::::u  r:::::r     r:::::ri::::i           Z:::::Z             z::::::z               J:::::J    aaaaaaa:::::a     x::::::::::x        x::::::::::x    
+       Y:::::Y       u::::u    u::::u  r:::::r     rrrrrrri::::i          Z:::::Z             z::::::z    JJJJJJJ     J:::::J  aa::::::::::::a      x::::::::x          x::::::::x     
+       Y:::::Y       u::::u    u::::u  r:::::r            i::::i         Z:::::Z             z::::::z     J:::::J     J:::::J a::::aaaa::::::a      x::::::::x          x::::::::x     
+       Y:::::Y       u:::::uuuu:::::u  r:::::r            i::::i      ZZZ:::::Z     ZZZZZ   z::::::z      J::::::J   J::::::Ja::::a    a:::::a     x::::::::::x        x::::::::::x    
+       Y:::::Y       u:::::::::::::::uur:::::r           i::::::i     Z::::::ZZZZZZZZ:::Z  z::::::zzzzzzzzJ:::::::JJJ:::::::Ja::::a    a:::::a    x:::::xx:::::x      x:::::xx:::::x   
+    YYYY:::::YYYY     u:::::::::::::::ur:::::r           i::::::i     Z:::::::::::::::::Z z::::::::::::::z JJ:::::::::::::JJ a:::::aaaa::::::a   x:::::x  x:::::x    x:::::x  x:::::x  
+    Y:::::::::::Y      uu::::::::uu:::ur:::::r           i::::::i     Z:::::::::::::::::Zz:::::::::::::::z   JJ:::::::::JJ    a::::::::::aa:::a x:::::x    x:::::x  x:::::x    x:::::x 
+    YYYYYYYYYYYYY        uuuuuuuu  uuuurrrrrrr           iiiiiiii     ZZZZZZZZZZZZZZZZZZZzzzzzzzzzzzzzzzzz     JJJJJJJJJ       aaaaaaaaaa  aaaaxxxxxxx      xxxxxxxxxxxxxx      xxxxxxx
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
+                                                                                                                                                                                       
 """)
