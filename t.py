e015274f2660482b8d6866265bb4f5bc1d4f5f62
yuriZzJaxx <132253210+yurizzjaxx@users.noreply.github.com>
2025-03-08 20:45:24 -0400

    Update t.py

--- t.py
+++ t.py
@@ -1,5 +1,15 @@
-def run(c):
-    print(c)
+import sys
 
-edit = input("code(##)")
-run(edit)
+def run(c):
+    if c == "code 2":
+        print("Executing code 2 command")
+        # Add your command logic here
+    else:
+        print(c)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        run(" ".join(sys.argv[1:]))
+    else:
+        edit = input("code(##)")
+        run(edit)
